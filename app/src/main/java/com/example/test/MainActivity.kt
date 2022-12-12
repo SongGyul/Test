a868c68735d5effd422b2bb7adc23e593e128419
gyul <gyul@hellomarket.com>
2022-12-12 23:33:20 +0900

    hi

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.test
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("TAG", ": ");
+        findViewById<Button>(R.id.btn).setOnClickListener {
+            Log.d("TAG", ": ");
+        }
     }
 }
